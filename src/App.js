ac8ec935cedbccfdc887b432f6c3ff2df7c09053
Ricardo Coradini <ricardocoradini97@gmail.com>
2025-02-02 12:16:06 -0300

    Segundo-comit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Header from './header/header';
+import Main from './Main/Main';
+import Footer from './footer/Footer';
+import Sidebar from './Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div>
+    <Header></Header>,
+    <Footer></Footer>,
+    <Main></Main>,
+    <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+
+export default App;
